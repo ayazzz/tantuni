b11e58524ac401cb04482b3edf11281e882e54f0
ayazzz <ayazzz@users.noreply.github.com>
2015-07-01 14:03:01 +0300

    Update esenyurtApp.js

--- esenyurtApp.js
+++ esenyurtApp.js
@@ -1,1 +1,150 @@
-var menuList={'kebaps':[{name:'',price:''},{name:'',price:''},{name:'',price:''},{name:'',price:''},{name:'',price:''}],'tantanas':[{name:'',price:''},{name:'',price:''},{name:'',price:''},{name:'',price:''},{name:'',price:''},{name:'',price:''},{name:'',price:''}]};
+var menus = {
+    'kebaps': [
+        {
+            name: 'Adana Dürüm',
+            price: '6.0',
+            p_name: 'Adana Dürüm Porsiyon',
+            p_price: '12.0',
+            img: 'assets/img/sp11.jpg',
+            order_no: 1
+        },
+        {
+            name: 'Ciğer Dürüm',
+            price: '7.0',
+            p_name: 'Ciğer Porsiyon',
+            p_price: '14.0',
+            img: 'assets/img/sp12.jpg',
+            order_no: 2
+        },
+        {
+            name: 'Kanat kg',
+            price: '22.0',
+            p_name: 'Kanat Porsiyon',
+            p_price: '12.0',
+            img: 'assets/img/sp13.jpg',
+            order_no: 3
+        },
+        {
+            name: 'Y.Ekmek Köfte',
+            price: '6.0',
+            p_name: 'Izgara Köfte Porsiyon',
+            p_price: '12.0',
+            img: 'assets/img/sp14.jpg',
+            order_no: 4
+        },
+        {
+            name: 'Kanat Şiş',
+            price: '12.0',
+            p_name: '',
+            p_price: '',
+            img: 'assets/img/sp13.jpg',
+            order_no: 5
+        },
+        {
+            name: 'Çöp Şiş Dürüm',
+            price: '7.0',
+            p_name: 'Çöp Şiş Porsiyon',
+            p_price: '14.0',
+            img: 'assets/img/sp15.jpg',
+            order_no: 6
+        },
+        {
+            name: 'Tavuk Şiş Dürüm',
+            price: '5.0',
+            p_name: 'Tavuk Şiş Porsiyon',
+            p_price: '10.0',
+            img: 'assets/img/sp17.jpg',
+            order_no: 7
+        },
+        {
+            name: 'Karışık Kebap',
+            price: '24.0',
+            p_name: '',
+            p_price: '',
+            img: 'assets/img/sp16.jpg',
+            order_no: 8
+        }
+    ],
+    'tantanas': [
+        {
+            name: 'Tantuni Dürüm',
+            price: '6.0',
+            p_name: '',
+            p_price: '',
+            img: 'assets/img/sp1.jpg',
+            order_no: 1
+        },
+        {
+            name: 'Tantuni Yarım',
+            price: '7.0',
+            p_name: '',
+            p_price: '',
+            img: 'assets/img/sp2.jpg',
+            order_no: 2
+        },
+        {
+            name: 'Ayvalık Tostu',
+            price: '5.50',
+            p_name: '',
+            p_price: '',
+            img: 'assets/img/sp3.jpg',
+            order_no: 3
+        },
+        {
+            name: 'Çorba',
+            price: '4.0',
+            p_name: '',
+            p_price: '',
+            img: 'assets/img/sp4.jpg',
+            order_no: 4
+        },
+        {
+            name: 'Patates Cipsi',
+            price: '4.0',
+            p_name: '',
+            p_price: '',
+            img: 'assets/img/sp5.jpg',
+            order_no: 5
+        },
+        {
+            name: 'Salata',
+            price: '4.0',
+            p_name: '',
+            p_price: '',
+            img: 'assets/img/sp6.jpg',
+            order_no: 6
+        },
+        {
+            name: 'Portakal Suyu',
+            price: '5.0',
+            p_name: '',
+            p_price: '',
+            img: 'assets/img/sp7.jpg',
+            order_no: 7
+        },
+        {
+            name: 'Nar Suyu',
+            price: '5.0',
+            p_name: '',
+            p_price: '',
+            img: 'assets/img/sp8.jpg',
+            order_no: 8
+        },
+        {
+            name: 'Susurluk Ayranı',
+            price: '2.0',
+            p_name: '',
+            p_price: '',
+            img: 'assets/img/sp9.jpg',
+            order_no: 9
+        },
+        {
+            name: 'Şalgam',
+            price: '2.0',
+            p_name: '',
+            p_price: '',
+            img: 'assets/img/sp10.jpg',
+            order_no: 10
+        }
+    ]
+};
